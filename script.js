638bd4d78ad0a2302b75aac640264febcd8bc99b
annupatil01 <anushkapatil16003@gmail.com>
2024-06-27 18:07:18 +0530

    foodlover is done

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("welcome to Foodlover")
+
+
+function order(){
+    alert("Order Placed")
+}
+function wrongfood(){
+    alert("Sorry, we don't have this food. Please try again")
+}
+function appdownload(){
+    alert("Download our app to get more offers")
+    alert("Are You Sure to Download")
+}
